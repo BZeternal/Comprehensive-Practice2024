c85096b7e1570825d44b4f65420f10a44c35776b
yxyu1 <2313288907@qq.com>
2024-05-21 22:23:06 +0800

    新增注释

--- backend/src/main/java/com/example/backend/pojo/Forum.java
+++ backend/src/main/java/com/example/backend/pojo/Forum.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,39 +22,46 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Forum implements Serializable {
     /**
-     * 
+     * 论坛id
      */
+    @JsonProperty("fId")
     @TableId(type = IdType.AUTO)
     private Integer fId;
 
     /**
-     * 
+     * 内容
      */
+    @JsonProperty("fContent")
     private String fContent;
 
     /**
-     * 
+     * 发表时间
      */
+    @JsonProperty("fTime")
     private Date fTime;
 
     /**
-     * 
+     * 题目
      */
+    @JsonProperty("fTitle")
     private String fTitle;
 
     /**
-     * 
+     * 发表图片
      */
+    @JsonProperty("fImg")
     private String fImg;
 
     /**
-     * 
+     * 点赞数量
      */
+    @JsonProperty("fNum")
     private Integer fNum;
 
     /**
-     * 
+     * 用户id
      */
+    @JsonProperty("uId")
     private Integer uId;
 
 }
